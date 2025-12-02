8a0c1c91a8f53b8ef7f313007fb094122d9136b6
sanuf.thaj <sanuf.thaj@acabes.com>
2025-12-02 12:21:45 +0530

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Bebas_Neue } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana  = Lusitana({ subsets: ['latin'], weight: '700' });
+export const bebeasFont = Bebas_Neue({ subsets: ['latin'], weight:'400'})
